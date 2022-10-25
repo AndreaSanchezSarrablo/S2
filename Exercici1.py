2467e5ac245cb865df38b62e9becba7668f717f7
Andrea Sánchez <72571689+AndreaSanchezSarrablo@users.noreply.github.com>
2022-10-25 22:50:31 +0200

    Update Exercici1.py

--- Exercici1.py
+++ Exercici1.py
@@ -4,6 +4,6 @@ import os
 #Ask the user for a number of seconds
 N = int(input('Enter the number of seconds you want form the video:'))
 
-#We cut N seconds from the video starting from the begining
+#We cut N seconds from the video starting from the begining, we are specifiying to cut the video from time = 0 to time = N seconds
 os.system("ffmpeg -i bbbVideo.mp4 -ss 0 -t " +str(N) + " -c:v copy -c:a copy cuttedVideo.mp4")
 
